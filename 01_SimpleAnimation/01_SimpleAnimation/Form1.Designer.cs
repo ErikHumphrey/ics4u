22512073476648ab6c39633435ec242f179c4c50
Erik Humphrey <ContactErikH@gmail.com>
2016-08-22 15:50:06 -0400

    Completed 01, Simple Animation

--- 01_SimpleAnimation/01_SimpleAnimation/Form1.Designer.cs
+++ 01_SimpleAnimation/01_SimpleAnimation/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace _01_SimpleAnimation
 {
-    partial class Form1
+    partial class frmSimpleAnimation
     {
         /// <summary>
         /// Required designer variable.
@@ -29,11 +29,72 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmSimpleAnimation));
+            this.pnlDisplay = new System.Windows.Forms.Panel();
+            this.tmrHover = new System.Windows.Forms.Timer(this.components);
+            this.btnStartStop = new System.Windows.Forms.Button();
+            this.picHelicopter = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picHelicopter)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlDisplay
+            // 
+            this.pnlDisplay.BackColor = System.Drawing.Color.White;
+            this.pnlDisplay.Location = new System.Drawing.Point(288, 12);
+            this.pnlDisplay.Name = "pnlDisplay";
+            this.pnlDisplay.Size = new System.Drawing.Size(313, 602);
+            this.pnlDisplay.TabIndex = 1;
+            // 
+            // tmrHover
+            // 
+            this.tmrHover.Interval = 50;
+            this.tmrHover.Tick += new System.EventHandler(this.tmrHover_Tick);
+            // 
+            // btnStartStop
+            // 
+            this.btnStartStop.Location = new System.Drawing.Point(51, 162);
+            this.btnStartStop.Name = "btnStartStop";
+            this.btnStartStop.Size = new System.Drawing.Size(193, 23);
+            this.btnStartStop.TabIndex = 3;
+            this.btnStartStop.Text = "Start / Stop";
+            this.btnStartStop.UseVisualStyleBackColor = true;
+            this.btnStartStop.Click += new System.EventHandler(this.btnStartStop_Click);
+            // 
+            // picHelicopter
+            // 
+            this.picHelicopter.Image = global::_01_SimpleAnimation.Properties.Resources.imgHelicopter_;
+            this.picHelicopter.Location = new System.Drawing.Point(34, 522);
+            this.picHelicopter.Name = "picHelicopter";
+            this.picHelicopter.Size = new System.Drawing.Size(230, 82);
+            this.picHelicopter.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picHelicopter.TabIndex = 2;
+            this.picHelicopter.TabStop = false;
+            // 
+            // frmSimpleAnimation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(613, 626);
+            this.Controls.Add(this.btnStartStop);
+            this.Controls.Add(this.picHelicopter);
+            this.Controls.Add(this.pnlDisplay);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmSimpleAnimation";
+            this.Text = "Simple Animation";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmSimpleAnimation_FormClosing);
+            this.Load += new System.EventHandler(this.frmSimpleAnimation_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picHelicopter)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel pnlDisplay;
+        private System.Windows.Forms.PictureBox picHelicopter;
+        private System.Windows.Forms.Timer tmrHover;
+        private System.Windows.Forms.Button btnStartStop;
     }
 }
 
--- 01_SimpleAnimation/01_SimpleAnimation/Form1.cs
+++ 01_SimpleAnimation/01_SimpleAnimation/Form1.cs
@@ -10,11 +10,58 @@ using System.Windows.Forms;
 
 namespace _01_SimpleAnimation
 {
-    public partial class Form1 : Form
+    public partial class frmSimpleAnimation : Form
     {
-        public Form1()
+        Graphics myGraphics;
+        int imageY;
+        int imageDir;
+        Brush blankBrush;
+
+        public frmSimpleAnimation()
         {
             InitializeComponent();
+        }
+
+        private void frmSimpleAnimation_Load(object sender, EventArgs e)
+        {
+            myGraphics = pnlDisplay.CreateGraphics();
+            blankBrush = new SolidBrush(pnlDisplay.BackColor);
+        }
+
+        private void frmSimpleAnimation_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            myGraphics.Dispose();
+            blankBrush.Dispose();
+        }
+
+        private void btnStartStop_Click(object sender, EventArgs e)
+        {
+            tmrHover.Enabled = !(tmrHover.Enabled);
+            imageY = 0;
+            imageDir = 1;
+        }
+
+        private void tmrHover_Tick(object sender, EventArgs e)
+        {
+            int imageX = 10;
+            int imageW = 100;
+            int imageH = 32;
+
+            imageX = (int)(0.5 * (pnlDisplay.Width - imageW));
+
+            myGraphics.FillRectangle(blankBrush, imageX, imageY, imageW, imageH);
+            imageY = imageY + imageDir * pnlDisplay.Height / 40;
+            myGraphics.DrawImage(picHelicopter.Image, imageX, imageY, imageW, imageH);
+            if (imageY + imageH > pnlDisplay.Height)
+            {
+                imageY = pnlDisplay.Height - imageH;
+                imageDir = -1;
+            }
+            else if (imageY < 0)
+            {
+                imageY = 0;
+                imageDir = 1;
+            }
         }
     }
 }
