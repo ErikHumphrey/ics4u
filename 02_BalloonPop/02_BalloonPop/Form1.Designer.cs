e7b987c7ec93df010debe409c0cc5f5ee42eac50
Erik Humphrey <ContactErikH@gmail.com>
2016-09-04 02:13:02 -0400

    Form double buffered to reduce flickering

--- 02_BalloonPop/02_BalloonPop/Form1.Designer.cs
+++ 02_BalloonPop/02_BalloonPop/Form1.Designer.cs
@@ -236,6 +236,7 @@
             this.ClientSize = new System.Drawing.Size(687, 502);
             this.Controls.Add(this.pnlGameView);
             this.Controls.Add(this.pnlUserInterface);
+            this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.KeyPreview = true;
