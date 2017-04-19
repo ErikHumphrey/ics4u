c3c152af23a96e10e311352202b92cc3e2536e46
Erik Humphrey <ContactErikH@gmail.com>
2017-04-19 07:49:16 -0400

    Create BackwardsName.java

---
+++ 2017/Module 5/BackwardsName.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class BackwardsName {
+    public static void main(String[] args) {
+        final int LOW = 'A'; //smallest possible value
+        final int HIGH = 'Z'; //highest possible value
+        int[] letterCounts = new int[HIGH - LOW + 1];
+        Scanner input = new Scanner(System.in);
+        String name;
+        char[] nameLetters;
+        int offset; //array index
+        /* Prompt user for their name: */
+        System.out.print("Enter your name: ");
+        name = input.nextLine();
+        /* convert word to char array and count letter occurrences */
+        name = word.toUpperCase();
+        wordLetters = word.toCharArray();
+        for (int letter = 0; letter < wordLetters.length; letter++) {
+            offset = wordLetters[letter] - LOW;
+            letterCounts[offset] += 1;
+        }
+        /* show letter occurrences */
+        for (int i = LOW; i <= HIGH; i++) {
+            System.out.println((char) i + ": " + letterCounts[i -
+                LOW]);
+        }
+    }
+}
