d90ac166e93f676fca84db7f662310bd84a30206
Erik Humphrey <ContactErikH@gmail.com>
2017-04-28 10:48:26 -0400

    .

--- 2017/Module 7/Humphrey, Erik - JavaFX 07, Keyboard UFO Game/src/GameAsn.java
+++ 2017/Module 7/Humphrey, Erik - JavaFX 07, Keyboard UFO Game/src/GameAsn.java
@@ -17,10 +17,11 @@ public class GameAsn extends Application {
         launch(args);
     }
 
-
+    public int gameStatus = 0;
+    
     public void start(Stage theStage) {
         theStage.setTitle("AnimationTimer Example");
-
+        
         Group root = new Group();
         Scene theScene = new Scene(root);
         theStage.setScene(theScene);
@@ -35,86 +36,156 @@ public class GameAsn extends Application {
         Image earth = new Image("earth.png");
         Image sun = new Image("sun.png");
         Image space = new Image("space.png");
-        Image ufo = new Image("ufo_0.png");
+        Image ufoBlue = new Image("ufo_blue.png");
+        Image ufoRed = new Image("ufo_red.png");
+        Image asteroid = new Image("asteroid.png");
         
         // Rectangle to hold place of UFO - starts near top-left
-        Rectangle ufoData = new Rectangle(10, 10, 25, 30);
+        Rectangle ufoBlueData = new Rectangle(10, 10, 20, 20);
+        Rectangle ufoRedData = new Rectangle(460, 460, 20, 20);
         Circle earthData = new Circle(10, 10, 22);
         Circle sunData = new Circle(260, 260, 50);
+        Circle asteroid1Data = new Circle(40, 80, 22);
+        Circle asteroid2Data = new Circle(80, 80, 22);
         
         // Only for testing
         // root.getChildren().add(earthData);
         // root.getChildren().add(sunData);
 
         final long startNanoTime = System.nanoTime(); // 60 times per second
+        gameStatus = 1;
 
         theScene.addEventFilter(KeyEvent.ANY, keyEvent -> {
+        	// Move the UFO up
+        	if (keyEvent.getCode() == KeyCode.W) {
+        		ufoBlueData.setY(ufoBlueData.getY() - 10);
+        	}
+        	
+        	// Move the UFO left
+        	if (keyEvent.getCode() == KeyCode.A) {
+        		ufoBlueData.setX(ufoBlueData.getX() - 10);
+        	}
+        	
+        	// Move the UFO down
+        	if (keyEvent.getCode() == KeyCode.S) {
+        		ufoBlueData.setY(ufoBlueData.getY() + 10);
+        	}
         	
         	// Move the UFO right
-        	if (keyEvent.getCode() == KeyCode.RIGHT) {
-        		ufoData.setX(ufoData.getX() + 10);
+        	if (keyEvent.getCode() == KeyCode.D) {
+        		ufoBlueData.setX(ufoBlueData.getX() + 10);
         	}
         	
         	// Move the UFO up
         	if (keyEvent.getCode() == KeyCode.UP) {
-        		ufoData.setY(ufoData.getY() - 10);
+        		ufoRedData.setY(ufoRedData.getY() - 10);
         	}
         	
         	// Move the UFO left
         	if (keyEvent.getCode() == KeyCode.LEFT) {
-        		ufoData.setX(ufoData.getX() - 10);
+        		ufoRedData.setX(ufoRedData.getX() - 10);
         	}
         	
         	// Move the UFO down
         	if (keyEvent.getCode() == KeyCode.DOWN) {
-        		ufoData.setY(ufoData.getY() + 10);
+        		ufoRedData.setY(ufoRedData.getY() + 10);
+        	}
+        	
+        	// Move the UFO right
+        	if (keyEvent.getCode() == KeyCode.RIGHT) {
+        		ufoRedData.setX(ufoRedData.getX() + 10);
         	}
         });
         
         new AnimationTimer() {
             public void handle(long currentNanoTime) {
-                double t = (currentNanoTime - startNanoTime) / 1000000000.0;
+                double t = (currentNanoTime - startNanoTime) / 500000000.0;
+                
+                double earthX = 232 + 128 * Math.cos(t) * 0.7;
+                double earthY = 232 + 128 * Math.sin(t) * 0.8;
 
-                double x = 232 + 128 * Math.cos(t); // Move the x coordinate on a cosine path
-                double y = 232 + 128 * Math.sin(t); // Move the y coordinate on a sine path
-
+                double asteroid1X = 232 + 200 * Math.cos(t) * -1;
+                double asteroid2X = 232 + 200 * Math.cos(t);
+                
+                double asteroid1Y = 232 + 200 * Math.sin(t) * -1;
+                double asteroid2Y = 232 + 200 * Math.sin(t);
+                
                 // Clear the canvas
                 gc.clearRect(0, 0, 512, 512);
 
-                x = x + 1;
+                earthX++;
+                asteroid1X++;
+                asteroid2Y++;
+                
+                switch (gameStatus) {
+                case 1:
+                	System.out.println("foo");
+                	gc.drawImage(earth, earthX, earthY);
+                	break;
+                case 2:
+                    gc.drawImage(asteroid, asteroid1X, asteroid1Y);
+                    gc.drawImage(asteroid, asteroid2X, asteroid2Y);
+                	break;
+                default:
+                    break;
+                }
+                
                 // Background image clear canvas
                 gc.drawImage(space, 0, 0); // Draw the background
-                gc.drawImage(earth, x, y); // Draw the earth in a new location each tick
+                // Draw the earth in a new location each tick
                 gc.drawImage(sun, 196, 196);
                 
-                // Move the earthData behind earth
-                earthData.setCenterX(x + 24);
-                earthData.setCenterY(y + 24);
+                // Move the data behind object
+                earthData.setCenterX(earthX + 24);
+                earthData.setCenterY(earthY + 24);
+                asteroid1Data.setCenterX(asteroid1X + 24);
+                asteroid1Data.setCenterY(asteroid1Y + 24);
+                asteroid2Data.setCenterX(asteroid2X + 24);
+                asteroid2Data.setCenterY(asteroid2Y + 24);
 
-                // Add the UFO
-                gc.drawImage(ufo, ufoData.getX(), ufoData.getY());
+                // Add the UFOs
+                gc.drawImage(ufoBlue, ufoBlueData.getX(), ufoBlueData.getY());
+                gc.drawImage(ufoRed, ufoRedData.getX(), ufoRedData.getY());
+                
+            	Alert alert = new Alert(AlertType.INFORMATION);
+            	alert.setTitle("Information Dialog");
+            	
+                final String fail = "Mission failed!";
                 
                 // Check if UFO is colliding with the Earth
-                if (ufoData.intersects(earthData.getBoundsInLocal())) {
-                	System.out.println("COLLISION");
-                	this.stop();
-                	
-                	Alert alert = new Alert(AlertType.INFORMATION);
-                	alert.setTitle("Information Dialog");
+                if (ufoBlueData.intersects(earthData.getBoundsInLocal())) {
+                	// this.stop();
                 	alert.setHeaderText("Collision!");
                 	alert.setContentText("Aliens have invaded earth");
                 	alert.show();
+                	gameStatus = 2;
+                }
+                if (ufoRedData.intersects(earthData.getBoundsInLocal())) {
+                	// this.stop();
+                	alert.setHeaderText("Collision!");
+                	alert.setContentText("Aliens have invaded earth");
+                	alert.show();
+                	gameStatus = 2;
                 }
                 
                 // Check if UFO is colliding with the Sun
-                if (ufoData.intersects(sunData.getBoundsInLocal())) {
-                	System.out.println("COLLISION");
+                if (ufoBlueData.intersects(sunData.getBoundsInLocal())) {
                 	this.stop();
-                	
-                	Alert alert = new Alert(AlertType.INFORMATION);
-                	alert.setTitle("Information Dialog");
-                	alert.setHeaderText("Mission failed!");
-                	alert.setContentText("Lost contact with space vessel\nCrew presumed dead");
+                	alert.setHeaderText(fail);
+                	alert.setContentText("Lost contact with blue vessel\nCrew presumed dead");
+                	alert.show();
+                }
+                if (ufoBlueData.intersects(sunData.getBoundsInLocal())) {
+                	this.stop();
+                	alert.setHeaderText(fail);
+                	alert.setContentText("Lost contact with red vessel\nCrew presumed dead");
+                	alert.show();
+                }
+                // Check if UFO is colliding with another UFO
+                if (ufoBlueData.intersects(ufoRedData.getBoundsInLocal())) {
+                	this.stop();
+                	alert.setHeaderText(fail);
+                	alert.setContentText("Lost contact with space vessels\nCrew presumed dead");
                 	alert.show();
                 }
             }
