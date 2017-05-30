ef0423f77779ad9d7f4351c775b940db85e41eaf
Erik Humphrey <ContactErikH@gmail.com>
2017-05-30 10:31:34 -0400

    ...

--- 2017/Hometown Hero/src/Main.java
+++ 2017/Hometown Hero/src/Main.java
@@ -1,8 +1,14 @@
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
 	public static void main(String[] args)  {
@@ -13,14 +19,27 @@ public class Main extends Application {
 	@Override
 	public void start(Stage mainWindow) {
 		mainWindow.setTitle("Hometown Hero");
+		mainWindow.setWidth(854);
+		mainWindow.setHeight(480);
+        mainWindow.centerOnScreen();
+		mainWindow.setResizable(false);
+
 		mainWindow.show();
 		Button btnStart = new Button("Start");
 		
-        GridPane layout = new GridPane();
-        layout.add(btnStart, 1, 4);
+		VBox vb = new VBox();
+		vb.setPadding(new Insets(10, 50, 50, 50));
+		vb.setSpacing(10);
 		
-        Scene scene = new Scene(layout, 350, 275);
+        // GridPane layout = new GridPane();
+        vb.getChildren().add(btnStart);
+		
+        Scene scene = new Scene(vb);
         mainWindow.setScene(scene);
+        
+        Text title = new Text("Placeholder");
+        title.setFont(Font.font("Arial", FontWeight.BOLD, 40));
+        vb.getChildren().add(title);
 	}
 
 }
