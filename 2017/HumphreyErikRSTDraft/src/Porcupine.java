b140b2a8815255b8df2362bb521ec0e26307d4ca
Erik Humphrey <ContactErikH@gmail.com>
2017-06-20 08:29:53 -0400

    Update Porcupine.java

--- 2017/HumphreyErikRSTDraft/src/Porcupine.java
+++ 2017/HumphreyErikRSTDraft/src/Porcupine.java
@@ -1,11 +1,21 @@
+// This enemy would act as a tutorial for ducking and attacking.
+// The porcupine is short enough that standing attacks would miss.
+// The porcupine would deal damage if bumped into, but it doesn't pursue the player.
+
 import javafx.scene.image.Image;
 
 public class Porcupine extends Enemy {
+	/** constructor
+	 * pre: none
+	 * post: Porcupine object created. Its health is intialized as 1. */
 	public Porcupine() {
 		health = 1;
 	}
 	
-	public String nextFrame() {
+	/** Returns the name of the class represented by the object as a String
+	 * pre: none
+	 * post: Class of the object has been returned as a string. */
+	public String getName() {
 		return getClass().getName();
 	}
 }
