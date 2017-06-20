1d442be258ec78395ddab19fcbf6b733581a6a10
Erik Humphrey <ContactErikH@gmail.com>
2017-06-20 08:06:31 -0400

    Update Bat.java

--- 2017/HumphreyErikRSTDraft/src/Bat.java
+++ 2017/HumphreyErikRSTDraft/src/Bat.java
@@ -1,6 +1,8 @@
+// Enemy that would act as a tutorial for jumping and attacking, as it is a flying bat
+
 import javafx.scene.image.Image;
 
-public class Bat extends Enemy {
+public class Bat extends Enemy { // Inherits Enemy class (consider making Enemy an interface instead of a class)
 	public Bat() {
 		health = 1;
 	}
